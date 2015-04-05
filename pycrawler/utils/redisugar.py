fbd1faba24f1500b4131fe903931db31db5252ad
pengmeng <mengp3157@gmail.com>
2015-04-04 23:59:07 -0500

    add redisugar.py

---
+++ pycrawler/utils/redisugar.py
@@ -0,0 +1,17 @@
+__author__ = 'mengpeng'
+import redis
+from pycrawler.exception import PyCrawlerException
+
+
+class RediSugar(object):
+    Pool = None
+
+    @staticmethod
+    def getConnection():
+        if not RediSugar.Pool:
+            RediSugar.Pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        r = redis.Redis(connection_pool=RediSugar.Pool)
+        try:
+            r.ping()
+        except redis.ConnectionError:
+            raise PyCrawlerException('Connect to redis server failed')
