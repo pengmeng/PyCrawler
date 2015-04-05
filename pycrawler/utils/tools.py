465fbc1657e90134323fd05ee4216da5af110ee4
pengmeng <mengp3157@gmail.com>
2015-04-04 20:00:45 -0500

    add tools

---
+++ pycrawler/utils/tools.py
@@ -0,0 +1,18 @@
+__author__ = 'mengpeng'
+import time
+
+
+def gethash(string, cap=0xffffffff):
+    return hash(string) & cap
+
+
+def timestamp():
+    return time.strftime("%H:%M:%S", time.localtime(time.time()))
+
+
+def datastamp():
+    return time.strftime("%Y-%m-%d", time.localtime(time.time()))
+
+
+def fullstamp():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
