a53c1fc4a7b2f67c47ad7b22d6e8c5c2c21ab563
pengmeng <mengp3157@gmail.com>
2015-04-05 20:18:32 -0500

    add frontier base class and BFSFrontier and test cases

--- pycrawler/frontier.py
+++ pycrawler/frontier.py
@@ -1,29 +1,144 @@
 __author__ = 'mengpeng'
+import re
+from pybloom import ScalableBloomFilter
+from pycrawler.exception import FrontierException
+from pycrawler.utils.redisugar import RediSugar
 
 
 class Frontier(object):
+    Dict = {}
 
-    def __init__(self):
+    def __init__(self, spider):
         pass
+
+    @staticmethod
+    def register(cls):
+        if isinstance(cls, type):
+            Frontier.Dict[cls.__name__] = cls
+            return cls
+        else:
+            raise FrontierException('Must register Frontier with class name')
+
+    @staticmethod
+    def getFrontier(name):
+        if name in Frontier.Dict:
+            return Frontier.Dict[name]
+        else:
+            raise FrontierException('No Frontier class named '+name)
+
+    def setargs(self, args):
+        raise NotImplementedError
 
     def __len__(self):
         raise NotImplementedError
 
-    def __iter__(self):
+    def __contains__(self, item):
         raise NotImplementedError
 
     def add(self, item):
         raise NotImplementedError
 
+    def next(self, *args):
+        raise NotImplementedError
 
-class DefaultFrontier(Frontier):
+    def hasnext(self):
+        raise NotImplementedError
 
-    def __init__(self, tasks=None):
-        super(DefaultFrontier, self).__init__()
-        self._tasks = tasks if tasks else []
+    def isVisited(self, item):
+        raise NotImplementedError
+
+    def validate(self, item):
+        raise NotImplementedError
+
+
+@Frontier.register
+class BFSFrontier(Frontier):
+    def __init__(self, spider):
+        super(BFSFrontier, self).__init__(spider)
+        self._spider = spider
+        self.args = {'filter': True,
+                     'rules': []}
+        self.redis = RediSugar.getConnection()
+        self.filter = ScalableBloomFilter(mode=ScalableBloomFilter.SMALL_SET_GROWTH)
+        self.todo = spider.name + '-todo'
+        self.visited = spider.name + '-visited'
+
+    def setargs(self, args):
+        if not isinstance(args, dict):
+            raise FrontierException('Args must be a dict')
+        for key, value in args.iteritems():
+            self.args[key] = value
+        if self.args['rules']:
+            for each in self.args['rules']:
+                try:
+                    re.compile(each)
+                except re.error:
+                    raise FrontierException('Wrong regular expression: \'{0}\''.format(each))
 
     def __len__(self):
-        return len(self._tasks)
+        return self.redis.llen(self.todo)
 
-    def __iter__(self):
-        return iter(self._tasks)
+    def __contains__(self, item):
+        temp = self.redis.lrange(self.todo, 0, self.__len__())
+        return item in temp
+
+    def add(self, item):
+        if isinstance(item, list):
+            for each in iter(item):
+                self._addone(each)
+        elif isinstance(item, str):
+            self._addone(item)
+        else:
+            raise FrontierException('Unsupported type: {0}'.format(type(item)))
+
+    def _addone(self, item):
+        if not self.isVisited(item) and self.validate(item):
+            self.redis.rpush(self.todo, item)
+
+    def next(self, num=1):
+        if num == 1:
+            return self._nextone()
+        elif num == 0 or num >= self.__len__():
+            return self._nextall()
+        elif num > 1:
+            result = []
+            while len(result) < num:
+                item = self._nextone()
+                if item:
+                    result.append(item)
+            return result
+        else:
+            raise FrontierException('Num should be greater than 0')
+
+    def _nextone(self):
+        item = self.redis.lpop(self.todo)
+        while item:
+            if item in self.filter:
+                item = self.redis.lpop(self.todo)
+            else:
+                self.filter.add(item)
+                self.redis.rpush(self.visited, item)
+                break
+        return item
+
+    def _nextall(self):
+        temp = self.redis.lrange(self.todo, 0, self.__len__())
+        result = [x for x in temp if x not in self.filter]
+        self.redis.ltrim(self.todo, len(temp), self.__len__())
+        for each in iter(result):
+            self.filter.add(each)
+            self.redis.rpush(self.visited, each)
+        return result
+
+    def hasnext(self):
+        return self.__len__() != 0
+
+    def isVisited(self, item):
+        return item in self.filter
+
+    def validate(self, item):
+        if self.args['rules']:
+            for each in self.args['rules']:
+                if not re.match(each, item):
+                    return False
+        return True
---
+++ test/test_frontier.py
@@ -0,0 +1,64 @@
+__author__ = 'mengpeng'
+from unittest import TestCase
+from pycrawler.frontier import Frontier
+from pycrawler.exception import FrontierException
+from test_scraper import SpiderTest
+
+
+class TestBFSFrontier(TestCase):
+    def test_setargs(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        args = {'rules': ['^http://', '[0-9]*']}
+        f.setargs(args)
+        self.assertEqual(2, len(f.args['rules']))
+        args = {'rules': ['[0-9]', '[--']}
+        self.assertRaises(FrontierException, f.setargs, args)
+
+    def test_add(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        urls = ['http://www.sample{0}.com'.format(x) for x in xrange(5)]
+        f.add(urls[0])
+        f.add(urls[1:])
+        for each in urls:
+            self.assertTrue(each in f)
+
+    def test__addone(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        url = 'http://www.google.com'
+        f._addone(url)
+        self.assertTrue(url in f)
+
+    def test_next(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        pass
+
+    def test__nextone(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        before = len(f)
+        if before == 0:
+            f.add('url-for-test-nextone')
+            before = 1
+        item = f._nextone()
+        after = len(f)
+        self.assertIsInstance(item, str)
+        self.assertEqual(1, f.filter.count)
+        self.assertEqual(1, before-after)
+        self.assertTrue(f.isVisited(item))
+
+    def test__nextall(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        before = f.filter.count
+        items = f._nextall()
+        after = f.filter.count
+        self.assertEqual(0, len(f))
+        self.assertEqual(len(items), after-before)
+        if items:
+            for each in items:
+                self.assertTrue(f.isVisited(each))
+
+    def test_validate(self):
+        f = Frontier.getFrontier('BFSFrontier')(SpiderTest('testspider'))
+        args = {'rules': [
+            '((http|ftp|https)://)(([a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(/[a-zA-Z0-9\&%_\./-~-]*)?']}
+        f.setargs(args)
+        self.assertTrue(f.validate('http://www.baidu.com'))
