651547bb0d2e4dc030f727620d41871bf5e565c9
pengmeng <mengp3157@gmail.com>
2015-04-10 01:52:18 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
+import os
 from setuptools import setup, find_packages
 
-version = '0.1'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+version = '0.0.1'
 
 setup(
     name='pycrawler',
@@ -11,11 +16,19 @@ setup(
     author='mengpeng',
     author_email='mengp3157@gmail.com',
     description='A lightweight Python crawler framework',
+    long_description=read('README.md'),
+    platforms='any',
     install_requires=['beautifulsoup4>=4.3.2',
                       'bitarray>=0.8.1',
                       'eventlet>=0.17.2',
                       'greenlet>=0.4.5',
                       'pybloom>=1.1',
                       'pymongo>=2.8',
-                      'redis>=2.10.3']
+                      'redis>=2.10.3'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
